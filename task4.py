47863181bf96663e53a79a561f854cd553142777
VladlShevchenko <72383990+VladlShevchenko@users.noreply.github.com>
2021-09-16 01:59:34 +0300

    Update task4.py

--- task4.py
+++ task4.py
@@ -5,16 +5,16 @@ def getMaxWeight(items, capacity):
     size = len(items)
     # create matrix
     list = [[0 for a in range(capacity + 1)] for i in range(size + 1)]
+
     # loop which fill the memorization table
-for i in range(size + 1):
-for a in range(capacity + 1):
+    for i in range(size + 1):
+    for a in range(capacity + 1):
             if i == 0 or a == 0:
                 list[i][a] = 0
-elif items[i - 1] <= a:
-list[i][a] = max(items[i - 1] + list[i - 1][a - items[i - 1]],
-list[i - 1][a])
+            elif items[i - 1] <= a:
+                list[i][a] = max(items[i - 1] + list[i - 1][a - items[i - 1]], list[i - 1][a])
             else:
-list[i][a] = list[i - 1][a]
+                list[i][a] = list[i - 1][a]
 
     return list[size][capacity]
 
@@ -24,13 +24,12 @@ def main():
     # create parser
     parser = argparse.ArgumentParser()
     parser.add_argument("capacity", help="Bag capacity W", type=int)
-parser.add_argument("weights", help="List of weights of each gold bar", nargs='*')
+    parser.add_argument("weights", help="List of weights of each gold bar", nargs='*')
     args = parser.parse_args()
 
     # call function
-    maxWeight = getMaxWeight([ast.literal_eval(weight) for weight in
-args.weights], args.capacity)
-print(f"Maximum weight of gold that fits into a knapsack is {maxWeight} with capacity of {args.capacity}")
+    maxWeight = getMaxWeight([ast.literal_eval(weight) for weight in args.weights], args.capacity)
+    print(f"Maximum weight of gold that fits into a knapsack is {maxWeight} with capacity of {args.capacity}")
 
 if __name__ == "__main__":
     main()

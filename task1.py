018d5712639f75ee9094dcbf60857b56d051ea9d
VladlShevchenko <72383990+VladlShevchenko@users.noreply.github.com>
2021-09-15 21:57:18 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,16 @@
+import argparse
 
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('firstN', type=int, help='first number')
+    parser.add_argument('op', type=str, help='arithmetic operator')
+    parser.add_argument('secondN', type=int, help='second number')
+
+    args = parser.parse_args()
+
+    result = str(args.firstN) + args.op + str(args.secondN)
+    print(eval(result))
+
+if __name__ == '__main__':
+    main()

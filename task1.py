45281e6c7adf85238fd30bd5b2ac4304749331a4
VladlShevchenko <72383990+VladlShevchenko@users.noreply.github.com>
2021-11-11 21:55:41 +0200

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,16 +1,29 @@
-import argparse
+import random
+import timeit
 
-def main():
-    parser = argparse.ArgumentParser()
+# with open("foo.txt", "w") as outFile:
+#     outFile.writelines(["{}\n".format(x) for x in random.sample(range(-10000000, 1000001), 5000000)])
 
-    parser.add_argument('firstN', type=int, help='first number')
-    parser.add_argument('op', type=str, help='arithmetic operator')
-    parser.add_argument('secondN', type=int, help='second number')
-
-    args = parser.parse_args()
-
-    result = str(args.firstN) + args.op + str(args.secondN)
-    print(eval(result))
-
-if __name__ == '__main__':
-    main()
+s = """
+with open("foo.txt", "r") as file:
+    x = file.readlines()
+    res = 0
+    for i in x:
+        if i.strip().isdigit():
+            res += int(i.strip())
+"""
+print(timeit.timeit(s, number=10))
+s = """
+with open("foo.txt", 'r') as file:
+    res = 0
+    for line in file:
+        if line.strip().isdigit():
+            res += int(line.strip())
+"""
+print(timeit.timeit(s, number=10))
+s = """
+with open("foo.txt",'r') as file:
+    x = (int(line.strip()) for line in file if line.strip().isdigit())
+    res = sum(x)
+"""
+print(timeit.timeit(s, number=10))

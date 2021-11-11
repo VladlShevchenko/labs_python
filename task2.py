4ed1c0e08e80a2fcec11f07390d91674c31879b0
VladlShevchenko <72383990+VladlShevchenko@users.noreply.github.com>
2021-11-11 22:38:31 +0200

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,31 +1,211 @@
-import argparse
+import json
+from datetime import datetime
 
 
-# create variables for operations
-MULTIPLY = "multiply"
-DIVIDE = "divide"
-ADD = "add"
-SUBSTRACT = "substract"
+class Pizza:
+    """Pizza class with getters, setters"""
 
-#declare method for choosing operation
-def chooseOperation(firstN, op, secondN):
-    result = "Wrong operation. Choose between: add, substract, multiply, divide"
-    if op == MULTIPLY:
-        result = firstN * secondN
-    elif op == DIVIDE:
-        result = firstN / secondN
-    elif op == ADD:
-        result = firstN + secondN
-    elif op == SUBSTRACT:
-        result = firstN - secondN
-    return result
+    def __init__(self, day, name, price, ingredients):
+        self.__day = day
+        self.__name = name
+        self.__price = price
+        self.__ingredients = ingredients
 
-#create parser
-parser = argparse.ArgumentParser()
+    @property
+    def day(self):
+        return self.__day
 
-# add operations to the parser
-parser.add_argument('op', type=str, help='arithmetic operator')
+    @day.setter
+    def day(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Wrong input type!")
+        self.__day = value
 
-parser.add_argument('firstN', type=int, help='first number') parser.add_argument('secondN', type=int, help='second number')
-args = parser.parse_args()
-print(chooseOperation(args.firstN, args.op, args.secondN))
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Wrong input type!")
+        self.__name = value
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Wrong input type!")
+        if value < 0:
+            raise ValueError("Wrong input value!")
+        self.__price = value
+
+    @property
+    def ingredients(self):
+        return self.__ingredients
+
+    @ingredients.setter
+    def ingredients(self, values):
+        if not all([isinstance(ingredient, str) for ingredient in values]):
+            raise TypeError("Wrong type of ingredient")
+        self.__ingredients = values
+
+    def __str__(self):
+        return f'Weekday: {self.day},\n pizza: name {self.name}\n price: {self.price}, \n ' \
+               f'ingredients: {self.ingredients}'
+
+
+class MondayPizza(Pizza):
+
+    def __str__(self):
+        return f'Monday,\n pizza: name {self.name}\n price: {self.price}, \n ' \
+               f'ingredients: {self.ingredients}'
+
+
+class TuesdayPizza(Pizza):
+
+    def __str__(self):
+        return f'Tuesday: {self.day},\n pizza: name {self.name}\n price: {self.price}, \n ' \
+               f'ingredients: {self.ingredients}'
+
+
+class WednesdayPizza(Pizza):
+    def __str__(self):
+        return f'Wednesday: {self.day},\n pizza: name {self.name}\n price: {self.price}, \n ' \
+               f'ingredients: {self.ingredients}'
+
+
+class ThursdayPizza(Pizza):
+    def __str__(self):
+        return f'Thursday: {self.day},\n pizza: name {self.name}\n price: {self.price}, \n ' \
+               f'ingredients: {self.ingredients}'
+
+
+class FridayPizza(Pizza):
+
+    def __str__(self):
+        return f'Friday: {self.day},\n pizza: name {self.name}\n price: {self.price}, \n ' \
+               f'ingredients: {self.ingredients}'
+
+
+class SaturdayPizza(Pizza):
+    def __str__(self):
+        return f'Saturday: {self.day},\n pizza: name {self.name}\n price: {self.price}, \n ' \
+               f'ingredients: {self.ingredients}'
+
+
+class SundayPizza(Pizza):
+    def __str__(self):
+        return f'Sunday: {self.day},\n pizza: name {self.name}\n price: {self.price}, \n ' \
+               f'ingredients: {self.ingredients}'
+
+
+class Customer:
+    """Customer class with getters and setters"""
+
+    def __init__(self, surname, name):
+        self.surname = surname
+        self.name = name
+
+    @property
+    def surname(self):
+        return self.__surname
+
+    @surname.setter
+    def surname(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Wrong type of surname!")
+
+        if len(value) <= 0:
+            raise ValueError("Surname must not be empty")
+
+        self.__surname = value
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Wrong type of customer name")
+
+        if len(value) <= 0:
+            raise ValueError("Customer name must not be empty")
+
+        self.__name = value
+
+
+class Order:
+    """Order class with add_product, del_product and sum_order methods"""
+
+    ingredients = {'mozzarella': 20,
+                   'ham': 40,
+                   'bacon': 30,
+                   'BBQ sauce': 10,
+                   'tomato': 15}
+
+    def __init__(self, new_customer=None, new_pizza=None):
+        if not isinstance(new_customer, Customer):
+            raise TypeError("Wrong type of customer")
+        self.new_customer = new_customer
+
+        if not isinstance(new_pizza, Pizza):
+            raise TypeError("Wrong type of pizza")
+        self.pizza = new_pizza
+
+    def add_ingredients(self):
+        print(self.ingredients)
+        for item in self.ingredients:
+            if input(f"Would you like to add {item} Y/N ?").lower() == "y":
+                self.pizza.ingredients.append(item)
+                self.pizza.price += self.ingredients[item]
+        self.write_data()
+
+    def write_data(self):
+        data = {'customer name': self.new_customer.name,
+                'day': self.pizza.day,
+                'name': self.pizza.name,
+                'price': self.pizza.price,
+                'ingredients': self.ingredients
+                }
+        with open("order.json", "w") as write_file:
+            json.dump(data, write_file)
+
+
+def fabric():
+    current_date = datetime.today().strftime('%A')
+
+    if current_date == "Monday":
+        return deserialization(MondayPizza)
+    elif current_date == "Tuesday":
+        return deserialization(TuesdayPizza)
+    elif current_date == "Wednesday":
+        return deserialization(WednesdayPizza)
+    elif current_date == "Thursday":
+        return deserialization(ThursdayPizza)
+    elif current_date == "Friday":
+        return deserialization(FridayPizza)
+    elif current_date == "Saturday":
+        return deserialization(SaturdayPizza)
+    elif current_date == "Sunday":
+        return deserialization(SundayPizza)
+
+
+def deserialization(pizzarito):
+    with open("pizza_of_the_day.json", 'r') as file:
+        pizza_of_the_day = json.load(file)
+    for pizza_toppings in pizza_of_the_day:
+        if pizza_toppings["day"] == datetime.today().strftime('%A'):
+            return pizzarito(pizza_toppings["day"], pizza_toppings["name"], pizza_toppings["price"],
+                             pizza_toppings["ingredients"])
+
+
+customer = Customer("Vladyslav", "Shevchenko")
+pizza = fabric()
+print(pizza)
+order = Order(customer, pizza)
+order.add_ingredients()
